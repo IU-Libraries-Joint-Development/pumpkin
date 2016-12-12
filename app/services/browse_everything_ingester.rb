fa952d444cf67ec2847a021286c7aa2861982711
Mark H. Wood <mwood@IUPUI.Edu>
2016-12-12 12:07:21 -0500

    Document the Upload from Server ingestor so the next victim doesn't need to work out what it does.

--- app/services/browse_everything_ingester.rb
+++ app/services/browse_everything_ingester.rb
@@ -1,6 +1,26 @@
+# Collect content from server-local or "cloud" storage using
+# browse-everything[https://github.com/projecthydra/browse-everything]
+# and attach it to a repository object.
+#
+# The content is temporarily stored in a configurable directory -- see
+# <tt>config[:upload_to]</tt>.
+
 class BrowseEverythingIngester
-  attr_reader :curation_concern, :upload_set_id, :actor, :file_info
+  # repository object to receive this content
+  attr_reader :curation_concern
+
+  # TBD
+  attr_reader :upload_set_id
+
+  # installs the content
+  attr_reader :actor
+
+  # locator for the content
+  attr_reader :file_info
+
   delegate :mime_type, :file_name, :file_path, to: :file_info
+
+  # Configure this instance, but take no action yet.
   def initialize(curation_concern, upload_set_id, actor, file_info)
     @curation_concern = curation_concern
     @upload_set_id = upload_set_id
@@ -8,6 +28,7 @@ class BrowseEverythingIngester
     @file_info = FileInfo.new(file_info.to_h)
   end
 
+  # Get the content, provide it to an installer, and clean up scratch storage.
   def save
     actor.create_metadata(curation_concern, {})
     actor.create_content(decorated_file)
@@ -16,6 +37,7 @@ class BrowseEverythingIngester
 
   private
 
+    # Return a handle to the local copy of the content.
     def file
       @file ||= File.open(downloaded_file_path)
     end
@@ -24,15 +46,19 @@ class BrowseEverythingIngester
       @decorated_file ||= IoDecorator.new(file, mime_type, file_name)
     end
 
+    # Return an object that knows how to access the content.
     def retriever
       @retriever ||= BrowseEverything::Retriever.new
     end
 
+    # Fetch the content to a scratch file.  Returns the path to the
+    # resulting file.  Subsequent calls return the same path.
     def downloaded_file_path
       target = Plum::config[:upload_to]
       @downloaded_file_path ||= retriever.download(file_info, target)
     end
 
+    # Remove our local scratch copy of the content.
     def cleanup_download
       File.delete(file.path)
     end

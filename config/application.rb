7d84dc37d96ac4ed3b5c2f6d0006eb090a195ff3
Daniel Pierce <dlpierce@indiana.edu>
2017-04-10 11:25:46 -0400

    Use middleware to compress responses.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module Pumpkin
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Gzip all responses.
+    config.middleware.use Rack::Deflater
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'

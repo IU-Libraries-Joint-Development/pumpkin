5a68e62c6d87517f125f91ea87579bddfa82f847
Daniel Pierce <dlpierce@indiana.edu>
2017-04-24 16:45:36 -0400

    Run all of our checks for the default check.

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -8,24 +8,35 @@ OkComputer.require_authentication(ENV["PMP_OK_USER"], ENV["PMP_OK_PASS"]) unless
 # Following checks execute after full initialization so that backend configuration values are available
 Rails.application.configure do
   config.after_initialize do
+    checks = OkComputer::CheckCollection.new("Standard Checks")
+    OkComputer::Registry.register "default", checks
+
+    checks.register "rails", OkComputer::DefaultCheck.new
+    checks.register"database", OkComputer::ActiveRecordCheck.new
+
     redis_url = Redis.current.client.options[:host]
     redis_port = Redis.current.client.options[:port]
-    OkComputer::Registry.register "redis", OkComputer::RedisCheck.new(host: redis_url, port: redis_port)
+    checks.register "redis", OkComputer::RedisCheck.new(host: redis_url, port: redis_port)
 
-    OkComputer::Registry.register "ruby", OkComputer::RubyVersionCheck.new
+    checks.register "ruby", OkComputer::RubyVersionCheck.new
 
-    OkComputer::Registry.register "cache", OkComputer::GenericCacheCheck.new
+    checks.register "cache", OkComputer::GenericCacheCheck.new
 
     iiif_url = Plum.config[:iiif_url]
-    OkComputer::Registry.register "iiif", OkComputer::HttpCheck.new(iiif_url)
+    checks.register "iiif", OkComputer::HttpCheck.new(iiif_url)
 
     solr_url = ActiveFedora.solr_config[:url]
-    OkComputer::Registry.register "solr", OkComputer::SolrCheck.new(solr_url)
+    checks.register "solr", OkComputer::SolrCheck.new(solr_url)
 
     fcrepo_url = ActiveFedora.fedora_config.credentials[:url]
     fedora_user = ActiveFedora.fedora_config.credentials[:user]
     fedora_password = ActiveFedora.fedora_config.credentials[:password]
     auth_options = [fedora_user, fedora_password]
-    OkComputer::Registry.register "fcrepo", IuDevOps::FcrepoCheck.new(fcrepo_url, 10, auth_options)
+    checks.register "fcrepo", IuDevOps::FcrepoCheck.new(fcrepo_url, 10, auth_options)
+
+    # TODO: Remove this when CheckCollection instances not setting registrant_name is fixed.
+    checks.collection.each do |check_name, check_obj|
+      check_obj.registrant_name = check_name
+    end
   end
 end

b5170f15323f747d0670cd69857fcfcd8764bd00
Mark H. Wood <mwood@IUPUI.Edu>
2018-02-09 17:14:45 -0500

    [HPT-1241] Save more WIP

--- app/models/scanned_resource.rb
+++ app/models/scanned_resource.rb
@@ -11,9 +11,11 @@ class ScannedResource < ActiveFedora::Base
   self.valid_child_concerns = []
 
   def to_solr(solr_doc = {})
+    field_name = ActiveFedora.index_field_mapper.solr_name("ordered_by",
+                                                           :symbol)
     super.tap do |doc|
-      doc[ActiveFedora.index_field_mapper.solr_name("ordered_by", :symbol)] ||= []
-      doc[ActiveFedora.index_field_mapper.solr_name("ordered_by", :symbol)] += send(:ordered_by_ids)
+      doc[field_name] ||= []
+      doc[field_name] += send(:ordered_by_ids)
     end
   end
 end

c13dd4e91cebf64d4c2e1fe7332e72cbcf1887df
Daniel Pierce <dlpierce@indiana.edu>
2018-02-20 09:38:42 -0500

    Spec for OCR download link, only show link when full_text contains data.

--- spec/controllers/downloads_controller_spec.rb
+++ spec/controllers/downloads_controller_spec.rb
@@ -1,19 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe DownloadsController do
-  let(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:admin) }
   let(:fileset) { FactoryGirl.build(:file_set) }
-  let(:doc) { File.open(Rails.root.join("spec", "fixtures", "files", "test.hocr")) }
+  let(:doc_path) { Rails.root.join("spec", "fixtures", "files", "test.hocr") }
+  let(:iodec) { Hydra::Derivatives::IoDecorator.new(doc_path, 'text/xml', File.basename(doc_path)) }
 
   before do
-    Hydra::Works::AddFileToFileSet.call(fileset, doc, :extracted_text)
+    sign_in user
+    Hydra::Works::AddFileToFileSet.call(fileset, iodec, :extracted_text)
   end
 
   describe "show" do
     context "requested extracted text" do
       it "renders the extracted text file" do
-        get :show, file: 'extracted_text'
-        expect(response.body).to eq doc
+        get :show, id: fileset, file: 'extracted_text'
+        expect(response.body).to eq File.read(doc_path, mode: 'rb')
+        expect(response.headers['Content-Length']).to eq "1714"
+        expect(response.headers['Accept-Ranges']).to eq "bytes"
       end
     end
   end

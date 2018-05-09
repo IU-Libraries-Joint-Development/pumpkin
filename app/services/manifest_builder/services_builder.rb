d16a76d0b24afe916f9ebd385c20d63dbc8b6a2b
Randall Floyd <rdfloyd@indiana.edu>
2018-05-09 16:31:40 -0400

    HPT-1327 Add ocr presentation service to manifest builder

--- app/services/manifest_builder/services_builder.rb
+++ app/services/manifest_builder/services_builder.rb
@@ -14,19 +14,33 @@ class ManifestBuilder
         record.class == AllCollectionsPresenter ||
         record.class == CollectionShowPresenter ||
         searchable? == 'disabled'
-      service_array = {
-        "@context"  => "http://iiif.io/api/search/0/context.json",
-        "@id"       => "#{root_url}search/#{record.id}",
-        "profile"   => "http://iiif.io/api/search/0/search",
-        "label"     => "Search within item."
-      }
-      manifest["service"] = [service_array]
+      search_service_array = search_service
+      ocr_service_array = ocr_service
+      manifest["service"] = [search_service_array, ocr_service_array]
     end
 
     private
 
       def searchable?
         record.full_text_searchable[0] unless record.full_text_searchable.nil?
+      end
+
+      def search_service
+        {
+          "@context"  => "http://iiif.io/api/search/0/context.json",
+          "@id"       => "#{root_url}search/#{record.id}",
+          "profile"   => "http://iiif.io/api/search/0/search",
+          "label"     => "Search within item."
+        }
+      end
+
+      def ocr_service
+        {
+          "@context"  => "http://www.w3.org/ns/anno.jsonld",
+          "@id"       => "#{root_url}ocr/#{record.id}",
+          "profile"   => "http://4science.it/api/ocr/0/annotationCollection",
+          "label"     => "OCR"
+        }
       end
   end
 end

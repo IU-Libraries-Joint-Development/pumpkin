fc954ff3c6c0da8938888a057e126fbdb96ee301
Adam Ploshay <aploshay@iu.edu>
2020-06-11 09:27:10 -0400

    [ESSI-918] Fix bug in purl routing

--- config/routes.rb
+++ config/routes.rb
@@ -115,8 +115,8 @@ Rails.application.routes.draw do
   get '/robots.:format' => 'pages#robots'
 
   # Purl redirects
-  get '/purl/*id', to: 'purl#default', as: 'default_purl'
   get '/purl/formats/:id', to: 'purl#formats', as: 'formats_purl'
+  get '/purl/*id', to: 'purl#default', as: 'default_purl'
 
   # iiif search API
   get '/search/:id', to: 'search#search', as: :search
